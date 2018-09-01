b8800b86e840685d52747e217237dddd219b54c9
ehren <eleonberger@gmail.com>
2018-08-31 23:09:02 -0500

    Added 2018-2019 schedule

--- app/src/main/java/com/afftonapps/bluesschedulewidget/SimpleWidgetProvider.java
+++ app/src/main/java/com/afftonapps/bluesschedulewidget/SimpleWidgetProvider.java
@@ -43,21 +43,96 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
 
             //define game list, based upon New_York Time
             ArrayList<Game> gameList = new ArrayList<>();
-            gameList.add(new Game(DateTime.parse("2017-3-13 22:00", formatter), "@ Los Angeles"));
-            gameList.add(new Game(DateTime.parse("2017-3-15 22:00", formatter), "@ Anaheim"));
-            gameList.add(new Game(DateTime.parse("2017-3-16 22:30", formatter), "@ San Jose"));
-            gameList.add(new Game(DateTime.parse("2017-3-18 21:00", formatter), "@ Arizona"));
-            gameList.add(new Game(DateTime.parse("2017-3-21 21:00", formatter), "@ Colorado"));
-            gameList.add(new Game(DateTime.parse("2017-3-23 20:00", formatter), "vs Vancouver"));
-            gameList.add(new Game(DateTime.parse("2017-3-25 19:00", formatter), "vs Calgary"));
-            gameList.add(new Game(DateTime.parse("2017-3-27 20:00", formatter), "vs Arizona"));
-            gameList.add(new Game(DateTime.parse("2017-3-29 22:30", formatter), "@ Arizona"));
-            gameList.add(new Game(DateTime.parse("2017-3-31 21:00", formatter), "@ Colorado"));
-            gameList.add(new Game(DateTime.parse("2017-4-2 16:00", formatter), "vs Nashville"));
-            gameList.add(new Game(DateTime.parse("2017-4-4 20:00", formatter), "vs Winnipeg"));
-            gameList.add(new Game(DateTime.parse("2017-4-6 19:30", formatter), "@ Florida"));
-            gameList.add(new Game(DateTime.parse("2017-4-8 19:00", formatter), "@ Carolina"));
-            gameList.add(new Game(DateTime.parse("2017-4-9 18:00", formatter), "vs Colorado"));
+            gameList.add(new Game(DateTime.parse("2018-09-18 20:30", formatter), "@ DAL"));
+            gameList.add(new Game(DateTime.parse("2018-09-19 20:00", formatter), "@ MIN"));
+            gameList.add(new Game(DateTime.parse("2018-09-21 20:00", formatter), "vs CBJ"));
+            gameList.add(new Game(DateTime.parse("2018-09-23 15:00", formatter), "@ CBJ"));
+            gameList.add(new Game(DateTime.parse("2018-09-25 20:00", formatter), "vs WSH"));
+            gameList.add(new Game(DateTime.parse("2018-09-28 20:00", formatter), "vs DAL"));
+            gameList.add(new Game(DateTime.parse("2018-09-30 15:00", formatter), "@ WSH"));
+            gameList.add(new Game(DateTime.parse("2018-10-04 20:00", formatter), "vs WPG"));
+            gameList.add(new Game(DateTime.parse("2018-10-06 20:00", formatter), "vs CHI"));
+            gameList.add(new Game(DateTime.parse("2018-10-11 20:00", formatter), "vs CGY"));
+            gameList.add(new Game(DateTime.parse("2018-10-13 20:30", formatter), "@ CHI"));
+            gameList.add(new Game(DateTime.parse("2018-10-14 19:00", formatter), "vs ANA"));
+            gameList.add(new Game(DateTime.parse("2018-10-17 19:00", formatter), "@ MTL"));
+            gameList.add(new Game(DateTime.parse("2018-10-20 19:00", formatter), "@ TOR"));
+            gameList.add(new Game(DateTime.parse("2018-10-22 20:00", formatter), "@ WPG"));
+            gameList.add(new Game(DateTime.parse("2018-10-25 20:00", formatter), "vs CBJ"));
+            gameList.add(new Game(DateTime.parse("2018-10-27 20:00", formatter), "vs CHI"));
+            gameList.add(new Game(DateTime.parse("2018-11-01 20:00", formatter), "vs VGK"));
+            gameList.add(new Game(DateTime.parse("2018-11-03 20:00", formatter), "vs MIN"));
+            gameList.add(new Game(DateTime.parse("2018-11-06 20:00", formatter), "vs CAR"));
+            gameList.add(new Game(DateTime.parse("2018-11-09 20:00", formatter), "vs SJS"));
+            gameList.add(new Game(DateTime.parse("2018-11-11 15:00", formatter), "vs MIN"));
+            gameList.add(new Game(DateTime.parse("2018-11-14 20:00", formatter), "@ CHI"));
+            gameList.add(new Game(DateTime.parse("2018-11-16 22:00", formatter), "@ VGK"));
+            gameList.add(new Game(DateTime.parse("2018-11-17 22:30", formatter), "@ SJS"));
+            gameList.add(new Game(DateTime.parse("2018-11-19 20:00", formatter), "vs LAK"));
+            gameList.add(new Game(DateTime.parse("2018-11-21 20:00", formatter), "@ NSH"));
+            gameList.add(new Game(DateTime.parse("2018-11-23 20:00", formatter), "vs NSH"));
+            gameList.add(new Game(DateTime.parse("2018-11-24 19:00", formatter), "vs WPG"));
+            gameList.add(new Game(DateTime.parse("2018-11-28 19:00", formatter), "@ DET"));
+            gameList.add(new Game(DateTime.parse("2018-11-30 21:00", formatter), "@ COL"));
+            gameList.add(new Game(DateTime.parse("2018-12-01 20:00", formatter), "@ ARI"));
+            gameList.add(new Game(DateTime.parse("2018-12-05 20:00", formatter), "vs EDM"));
+            gameList.add(new Game(DateTime.parse("2018-12-07 20:00", formatter), "@ WPG"));
+            gameList.add(new Game(DateTime.parse("2018-12-09 15:00", formatter), "vs VAN"));
+            gameList.add(new Game(DateTime.parse("2018-12-11 20:00", formatter), "vs FLA"));
+            gameList.add(new Game(DateTime.parse("2018-12-14 20:00", formatter), "vs COL"));
+            gameList.add(new Game(DateTime.parse("2018-12-16 15:00", formatter), "vs CGY"));
+            gameList.add(new Game(DateTime.parse("2018-12-18 21:00", formatter), "@ EDM"));
+            gameList.add(new Game(DateTime.parse("2018-12-20 22:00", formatter), "@ VAN"));
+            gameList.add(new Game(DateTime.parse("2018-12-22 16:00", formatter), "@ CGY"));
+            gameList.add(new Game(DateTime.parse("2018-12-27 20:00", formatter), "vs BUF"));
+            gameList.add(new Game(DateTime.parse("2018-12-29 20:00", formatter), "vs PIT"));
+            gameList.add(new Game(DateTime.parse("2018-12-31 19:00", formatter), "vs NYR"));
+            gameList.add(new Game(DateTime.parse("2019-01-03 20:00", formatter), "vs WSH"));
+            gameList.add(new Game(DateTime.parse("2019-01-05 20:00", formatter), "vs NYI"));
+            gameList.add(new Game(DateTime.parse("2019-01-07 19:00", formatter), "@ PHI"));
+            gameList.add(new Game(DateTime.parse("2019-01-08 20:00", formatter), "vs DAL"));
+            gameList.add(new Game(DateTime.parse("2019-01-10 20:00", formatter), "vs MTL"));
+            gameList.add(new Game(DateTime.parse("2019-01-12 21:00", formatter), "@ DAL"));
+            gameList.add(new Game(DateTime.parse("2019-01-14 19:00", formatter), "@ WSH"));
+            gameList.add(new Game(DateTime.parse("2019-01-15 19:00", formatter), "@ NYI"));
+            gameList.add(new Game(DateTime.parse("2019-01-17 19:00", formatter), "@ BOS"));
+            gameList.add(new Game(DateTime.parse("2019-01-19 19:00", formatter), "vs OTT"));
+            gameList.add(new Game(DateTime.parse("2019-01-21 16:00", formatter), "@ LAK"));
+            gameList.add(new Game(DateTime.parse("2019-01-23 22:00", formatter), "@ ANA"));
+            gameList.add(new Game(DateTime.parse("2019-02-02 19:00", formatter), "@ CBJ"));
+            gameList.add(new Game(DateTime.parse("2019-02-05 19:00", formatter), "@ FLA"));
+            gameList.add(new Game(DateTime.parse("2019-02-07 19:30", formatter), "@ TBL"));
+            gameList.add(new Game(DateTime.parse("2019-02-09 14:00", formatter), "vs NSH"));
+            gameList.add(new Game(DateTime.parse("2019-02-10 12:30", formatter), "@ NSH"));
+            gameList.add(new Game(DateTime.parse("2019-02-12 20:00", formatter), "vs NJD"));
+            gameList.add(new Game(DateTime.parse("2019-02-14 21:00", formatter), "@ ARI"));
+            gameList.add(new Game(DateTime.parse("2019-02-16 15:00", formatter), "@ COL"));
+            gameList.add(new Game(DateTime.parse("2019-02-17 15:00", formatter), "@ MIN"));
+            gameList.add(new Game(DateTime.parse("2019-02-19 20:00", formatter), "vs TOR"));
+            gameList.add(new Game(DateTime.parse("2019-02-21 20:30", formatter), "@ DAL"));
+            gameList.add(new Game(DateTime.parse("2019-02-23 16:00", formatter), "vs BOS"));
+            gameList.add(new Game(DateTime.parse("2019-02-24 19:00", formatter), "@ MIN"));
+            gameList.add(new Game(DateTime.parse("2019-02-26 20:00", formatter), "vs NSH"));
+            gameList.add(new Game(DateTime.parse("2019-03-01 19:30", formatter), "@ CAR"));
+            gameList.add(new Game(DateTime.parse("2019-03-02 20:00", formatter), "vs DAL"));
+            gameList.add(new Game(DateTime.parse("2019-03-06 22:00", formatter), "@ ANA"));
+            gameList.add(new Game(DateTime.parse("2019-03-07 22:30", formatter), "@ LAK"));
+            gameList.add(new Game(DateTime.parse("2019-03-09 16:00", formatter), "@ SJS"));
+            gameList.add(new Game(DateTime.parse("2019-03-12 20:00", formatter), "vs ARI"));
+            gameList.add(new Game(DateTime.parse("2019-03-14 19:30", formatter), "@ OTT"));
+            gameList.add(new Game(DateTime.parse("2019-03-16 13:00", formatter), "@ PIT"));
+            gameList.add(new Game(DateTime.parse("2019-03-17 17:00", formatter), "@ BUF"));
+            gameList.add(new Game(DateTime.parse("2019-03-19 20:00", formatter), "vs EDM"));
+            gameList.add(new Game(DateTime.parse("2019-03-21 20:00", formatter), "vs DET"));
+            gameList.add(new Game(DateTime.parse("2019-03-23 20:00", formatter), "vs TBL"));
+            gameList.add(new Game(DateTime.parse("2019-03-25 20:00", formatter), "vs VGK"));
+            gameList.add(new Game(DateTime.parse("2019-03-29 19:00", formatter), "@ NYR"));
+            gameList.add(new Game(DateTime.parse("2019-03-30 19:00", formatter), "@ NJD"));
+            gameList.add(new Game(DateTime.parse("2019-04-01 20:00", formatter), "vs COL"));
+            gameList.add(new Game(DateTime.parse("2019-04-03 20:00", formatter), "@ CHI"));
+            gameList.add(new Game(DateTime.parse("2019-04-04 20:00", formatter), "vs PHI"));
+            gameList.add(new Game(DateTime.parse("2019-04-06 16:00", formatter), "vs VAN"));
+
 
             //find the next game by looping through
             Game nextGame = new Game();
@@ -168,10 +243,11 @@ public class SimpleWidgetProvider extends AppWidgetProvider {
         //test if the date is the same as today's date plus 1, if so return "Tomorrow"
         } else if ( g.gameTime.toDateTime( DateTimeZone.getDefault()).dayOfMonth().getAsText().equalsIgnoreCase( stringTomorrowDay ) ){
             returnString = "Tomorrow";
-        //if the game is neither today, tomorrow, or greater than a week from now, then use the day name, i.e. Wed
+        //if the game is neither today or tomorrow but less than a week from now, then use the day name, i.e. Wed
         } else {
             returnString = g.gameTime.toDateTime( DateTimeZone.getDefault()).toString("EEE");
         }
+        //TBD need to add another line to test if the date is > 7 day away and  if so display the date as a number
 //        Log.d("myTag","getDayString returning: " + returnString);
         return returnString;
     }
